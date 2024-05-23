100cdce082a24d9e6328432d37a7622a28b7cf4a
maria.clara <mariaclaracaltranbariotto@gmail.com>
2024-05-23 16:27:02 -0300

    Aula Dia 23/05

--- app/src/main/java/com/example/paginalogin/view/MainActivity2.java
+++ app/src/main/java/com/example/paginalogin/view/MainActivity2.java
@@ -37,7 +37,8 @@ public class MainActivity2 extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_produtos)
+                R.id.nav_home, R.id.nav_produtos, R.id.nav_acompanhamento, R.id.nav_atendimento, R.id.nav_fale_conosco,
+                R.id.nav_sobre_nos, R.id.nav_solicitacao)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main2);
