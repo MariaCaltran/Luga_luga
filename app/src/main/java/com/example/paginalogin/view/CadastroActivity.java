e02b21929b34f40ebbbe2ebf030546ae81fd18c9
maria.clara <mariaclaracaltranbariotto@gmail.com>
2024-05-28 16:34:08 -0300

    Aula Dia 28/05

--- app/src/main/java/com/example/paginalogin/view/CadastroActivity.java
+++ app/src/main/java/com/example/paginalogin/view/CadastroActivity.java
@@ -20,7 +20,9 @@ public class CadastroActivity extends AppCompatActivity {
 
     private Spinner spinner;
     private TextInputLayout inputCpf;
+    private TextInputLayout inputcep;
 
+    private TextInputLayout inputdata;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,13 +33,17 @@ public class CadastroActivity extends AppCompatActivity {
         setSupportActionBar(myToolbar);
 
         inputCpf = findViewById(R.id.input_cpf);
+        inputcep = findViewById(R.id.input_cep);
+        inputdata = findViewById(R.id.input_nascimento);
         inputCpf.getEditText().addTextChangedListener(new TextWatcher() {
+
 
             private static final String maskCPF = "###.###.###-##";
 
             boolean isUpdating;
 
             String old = "";
+
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
@@ -48,30 +54,124 @@ public class CadastroActivity extends AppCompatActivity {
                 String str = s.toString().replaceAll("[^0-9]*", "");
                 String mask = maskCPF;
 
-                    String mascara = "";
-                    if (isUpdating){
-                        old = str;
-                        isUpdating = false;
-                        return;
+                String mascara = "";
+                if (isUpdating) {
+                    old = str;
+                    isUpdating = false;
+                    return;
                 }
-                    int i = 0;
-                    for (char m : mask.toCharArray()) {
-                        if ((m != '#' && str.length() > old.length()) || (m != '#' && str.length() < old.length() && str.length() != i)){
-                            mascara += m;
-                            continue;
+                int i = 0;
+                for (char m : mask.toCharArray()) {
+                    if ((m != '#' && str.length() > old.length()) || (m != '#' && str.length() < old.length() && str.length() != i)) {
+                        mascara += m;
+                        continue;
 
-                        }
-
-                        try {
-                            mascara += str.charAt(i);
-                        } catch (Exception e){
-                            break;
-                        }
-                        i++;
                     }
-                    isUpdating = true;
-                    inputCpf.getEditText().setText(mascara);
-                    inputCpf.getEditText().setSelection(mascara.length());
+
+                    try {
+                        mascara += str.charAt(i);
+                    } catch (Exception e) {
+                        break;
+                    }
+                    i++;
+                }
+                isUpdating = true;
+                inputCpf.getEditText().setText(mascara);
+                inputCpf.getEditText().setSelection(mascara.length());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        inputcep.getEditText().addTextChangedListener(new TextWatcher() {
+            private static final String maskCEP = "#####-###";
+            boolean isUpdating;
+            String old = "";
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String str = s.toString().replaceAll("[^0-9]*", "");
+                String mask = maskCEP;
+
+                String mascara = "";
+                if (isUpdating) {
+                    old = str;
+                    isUpdating = false;
+                    return;
+                }
+                int i = 0;
+                for (char m : mask.toCharArray()) {
+                    if ((m != '#' && str.length() > old.length()) || (m != '#' && str.length() < old.length() && str.length() != i)) {
+                        mascara += m;
+                        continue;
+
+                    }
+
+                    try {
+                        mascara += str.charAt(i);
+                    } catch (Exception e) {
+                        break;
+                    }
+                    i++;
+                }
+                isUpdating = true;
+                inputcep.getEditText().setText(mascara);
+                inputcep.getEditText().setSelection(mascara.length());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        inputdata.getEditText().addTextChangedListener(new TextWatcher() {
+            private static final String maskDATA = "##/##/####";
+            boolean isUpdating;
+            String old = "";
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String str = s.toString().replaceAll("[^0-9]*", "");
+                String mask = maskDATA;
+
+                String mascara = "";
+                if (isUpdating) {
+                    old = str;
+                    isUpdating = false;
+                    return;
+                }
+                int i = 0;
+                for (char m : mask.toCharArray()) {
+                    if ((m != '#' && str.length() > old.length()) || (m != '#' && str.length() < old.length() && str.length() != i)) {
+                        mascara += m;
+                        continue;
+
+                    }
+
+                    try {
+                        mascara += str.charAt(i);
+                    } catch (Exception e) {
+                        break;
+                    }
+                    i++;
+                }
+                isUpdating = true;
+                inputdata.getEditText().setText(mascara);
+                inputdata.getEditText().setSelection(mascara.length());
             }
 
             @Override
